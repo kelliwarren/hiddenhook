92e219e6d77f4c12e963f8433f0a1138ec68a9a6
kelliwarren <kmdesigngroup@gmail.com>
2014-04-04 15:55:05 -0700

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hiddenhook::Application.config.secret_key_base = 'd1e061542fa6c54f077085a82b749fdb4a1217026c1ca4f8c55cc6bae679a1df0b181e9e0bd302b51fd63c7aeabf36b634a72f6aa5d281e81bc7dab8a9b8f1d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hiddenhook::Application.config.session_store :cookie_store, key: '_hiddenhook_session'
