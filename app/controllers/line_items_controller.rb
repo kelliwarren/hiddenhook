954f1a109d4df531bd9344b606bb011be38303b9
kelliwarren <kmdesigngroup@gmail.com>
2014-04-07 12:11:06 -0700

    line_items controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,24 +1,14 @@
 class LineItemsController < PublicController
+  belongs_to :order
 
-def create
-  @product = Product.find(params[:product_id])
-  @order = Order.find(session[:order_id])
-  line_item = @order.line_items.build(params[:line_item])
-  line_item.copy_product_attributes(@product)
-  line_item.save
-  redirect_to @product, notice: 'Added to order'
+  def create
+    @product = Product.find(params[:product_id])
+    @order = Order.find(session[:order_id])
+    line_item = @order.line_items.build(params[:line_item])
+    line_item.copy_product_attributes(@product)
+    line_item.save
+    redirect_to @product, notice: 'Added to order'
 
-end
-
-  #make a create action
-  #find the product by params product_id
-  #find the order.find by  session order_id...
-  #appends line item to order   order.line_items.build
-  #create a new line item
-  #copy appropriate fields from product to line item
-  #save line_item
-  #redirect user to checkout page
-
-
+  end
 
 end
