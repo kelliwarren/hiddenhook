878cab5dbc9aa18297000cd97d929d5b74654107
kelliwarren <kmdesigngroup@gmail.com>
2014-04-07 12:25:22 -0700

    sanitize params

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,15 +1,22 @@
 class LineItemsController < PublicController
-  
+
 
 
   def create
     @product = Product.find(params[:product_id])
     @order = Order.find(session[:order_id])
-    line_item = @order.line_items.build(params[:line_item])
+    line_item = @order.line_items.build(line_item_params)
     line_item.copy_product_attributes(@product)
     line_item.save
     redirect_to @product, notice: 'Added to order'
 
+  end
+
+
+  private
+
+  def def line_item_params
+    params.require(:line_item).permit(:name, :price)
   end
 
 end
